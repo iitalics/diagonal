566fae1ba5e5c79af2e165d552fe45db930ddf16
Milo Turner <iitalics@gmail.com>
2020-06-03 01:23:44 -0400

    oh delete that

--- dia_js/dia.ml
+++ dia_js/dia.ml
@@ -1,21 +1,3 @@
-(* generic drawing interface *)
-
-module type Draw_S = sig
-  module Color: sig
-    type t
-    val none: t
-    val of_rgb_s: string -> t
-  end
-
-  module Ctxt: sig
-    type t
-    val size: t -> int * int
-    val clear: f:Color.t -> t -> unit
-  end
-end
-
-(* drawing implementation *)
-
 module Dom = Js_of_ocaml.Dom
 module Dom_html = Js_of_ocaml.Dom_html
 module Js = Js_of_ocaml.Js
