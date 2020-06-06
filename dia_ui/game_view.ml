0c01e4500c9676ec7215a134725d1ab6faa47bcf
Milo Turner <iitalics@gmail.com>
2020-06-06 18:55:35 -0400

    game_view stub

---
+++ dia_ui/game_view.ml
@@ -0,0 +1,49 @@
+module type S =
+  View.S with type init = unit
+
+module Make
+         (Draw: Intf.Draw_S)
+         (Rsrc: Intf.Rsrc_S with type font = Draw.Font.t
+                             and type image = Draw.Image.t)
+         (View_disp: View.Dispatcher_S with type draw_ctxt = Draw.Ctxt.t
+                                        and type 'a rsrc = 'a Rsrc.t)
+       : S with type 'a rsrc = 'a Rsrc.t
+            and type view_disp = View_disp.t
+            and type draw_ctxt = Draw.Ctxt.t
+  =
+  struct
+    type 'a rsrc = 'a Rsrc.t
+    type view_disp = View_disp.t
+    type draw_ctxt = Draw.Ctxt.t
+
+    (*** assets ***)
+
+    type assets =
+      { sprites: Draw.Image.t;
+        map: Draw.Image.t }
+
+    let assets_rsrc =
+      Rsrc.map2 (fun sprites map -> { sprites; map })
+        (Rsrc.image ~path:"sprites")
+        (Rsrc.image ~path:"map_stone")
+
+    (*** init ***)
+
+    type t =
+      { assets: assets } [@@ocaml.unboxed]
+
+    type init = unit
+    let make assets _init =
+      { assets }
+
+    (*** event handling ***)
+
+    let handle_evt _ _ = ()
+    let switch _disp _v = ()
+
+    (*** rendering ***)
+
+    let bg_f = Draw.Color.of_rgb_s "#5cf"
+    let render cx _v = cx |> Draw.Ctxt.clear ~f:bg_f
+
+  end
--- dia_ui/ui.ml
+++ dia_ui/ui.ml
@@ -1,86 +1,104 @@
 module Make_views
          (Draw: Intf.Draw_S)
-         (Rsrc: Intf.Rsrc_S with type font = Draw.Font.t)
+         (Rsrc: Intf.Rsrc_S with type font = Draw.Font.t
+                             and type image = Draw.Image.t)
          (View_disp: View.Dispatcher_S with type draw_ctxt = Draw.Ctxt.t
                                         and type 'a rsrc = 'a Rsrc.t)
   =
   struct
     module type View_S = View_disp.View_S
 
-    module Green_screen: View_S with type init = unit =
+    module Game_view = Game_view.Make(Draw)(Rsrc)(View_disp)
+
+    (*** assets common to menu screens ***)
+
+    type menu_assets =
+      { title_font: Draw.Font.t;
+        item_font: Draw.Font.t }
+
+    module Menu_assets = struct
+      type assets = menu_assets
+      let assets_rsrc =
+        Rsrc.map2 (fun title_font item_font -> { title_font; item_font })
+          (Rsrc.font ~family:"roundor" ~size:100)
+          (Rsrc.font ~family:"nunito" ~size:30)
+    end
+
+    (*** main menu! ***)
+
+    module Main_menu: View_S with type init = unit and type assets = menu_assets =
       struct
         type view_disp = View_disp.t
         type draw_ctxt = Draw.Ctxt.t
         type 'a rsrc = 'a Rsrc.t
-
-        (* assets *)
-
-        type assets = unit
-        let assets_rsrc = Rsrc.const ()
+        include Menu_assets
 
         (* init *)
 
-        type t = bool ref
+        type t =
+          { assets: assets;
+            mutable hov: int;
+            mutable sel: bool }
+
         type init = unit
-        let make _assets _init = ref false
+        let make assets _init =
+          { assets;
+            hov = 0;
+            sel = false }
 
         (* event handling *)
 
-        let handle_evt (evt: View.Evt.t) v = match evt with
-          | Key_dn("Escape") -> v := true
-          | Key_dn(x) -> Printf.printf "key dn: %S\n" x
+        let handle_evt ev v = match (ev : View.Evt.t) with
+          | Key_dn "ArrowDown" -> v.hov <- v.hov + 1
+          | Key_dn "ArrowUp"   -> v.hov <- v.hov - 1
+          | Key_dn "Enter"     -> v.sel <- true
           | _ -> ()
 
-        let switch vd v = if !v then vd |> View_disp.pop_view
+        let switch disp v =
+          if v.sel then
+            ( v.sel <- false;
+              match v.hov with
+              | 0 -> disp |> View_disp.push_view (module Game_view)
+                               ~init:()
+              | _ -> () )
 
         (* rendering *)
 
-        let bg_f = Draw.Color.of_rgb_s "#8f0"
-        let render cx _v = cx |> Draw.Ctxt.clear ~f:bg_f
+        let bg_f       = Draw.Color.of_rgb_s "#8df"
+        let title_f    = Draw.Color.of_rgb_s "#000"
+        let item_f     = Draw.Color.of_rgb_s "#fff"
+        let item_hov_f = Draw.Color.of_rgb_s "#064"
+
+        let title_ypad = 20
+        let item_ypad = 8
+
+        let title_text = "Diag"
+        let item_text = [ "Single player mode";
+                          "Change Controls";
+                          "Change Character" ]
+
+        let render_title assets (w, h) cx =
+          let (mes_w, mes_h) = assets.title_font |> Draw.Font.measure title_text in
+          let x, y = (w - mes_w) / 2, (h - mes_h) / 2 in
+          cx |> Draw.Ctxt.text title_text
+                  ~x ~y ~f:title_f ~font:assets.title_font;
+          let y = y + mes_h + title_ypad in
+          y
+
+        let render_item assets hov_i (w, _) cx (i, y) text =
+          let (mes_w, mes_h) = assets.item_font |> Draw.Font.measure text in
+          let x = (w - mes_w) / 2 in
+          cx |> Draw.Ctxt.text text
+                  ~x ~y ~font:assets.item_font
+                  ~f:(if i = hov_i then item_hov_f else item_f);
+          let y = y + mes_h + item_ypad in
+          (i + 1, y)
+
+        let render cx v =
+          let size = cx |> Draw.Ctxt.size in
+          cx |> Draw.Ctxt.clear ~f:bg_f;
+          let y = cx |> render_title v.assets size in
+          let y = List.fold_left (render_item v.assets v.hov size cx) (0, y) item_text in
+          ignore y
       end
-
-
-    module Main_menu: View_S with type init = unit =
-      struct
-        type view_disp = View_disp.t
-        type draw_ctxt = Draw.Ctxt.t
-        type 'a rsrc = 'a Rsrc.t
-
-        (* assets *)
-
-        type assets =
-          { title_font: Draw.Font.t;
-            item_font: Draw.Font.t }
-
-        let assets_rsrc =
-          Rsrc.map2 (fun title_font item_font -> { title_font; item_font })
-            (Rsrc.font ~family:"roundor" ~size:100)
-            (Rsrc.font ~family:"roundor" ~size:30)
-
-        (* init *)
-
-        type t = { assets: assets; mutable switch: bool }
-        type init = unit
-        let make assets _init = { assets; switch = false }
-
-        (* event handling *)
-
-        let handle_evt (evt: View.Evt.t) (v: t) = match evt with
-          | Key_dn("g") -> v.switch <- true
-          | _ -> ()
-
-        let switch vd (v: t) =
-          if v.switch then
-            ( v.switch <- false;
-              vd |> View_disp.push_view
-                      (module Green_screen)
-                      ~init:() )
-
-        (* rendering *)
-
-        let bg_f = Draw.Color.of_rgb_s "#8df"
-        let render cx _v =
-          cx |> Draw.Ctxt.clear ~f:bg_f
-      end
-
   end
