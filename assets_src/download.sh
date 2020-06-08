e3f0976c2d131eb013198aa6bf737b16cea0240d
Milo Turner <iitalics@gmail.com>
2020-06-08 17:36:55 -0400

    "space mono bold" font

--- assets_src/download.sh
+++ assets_src/download.sh
@@ -1,6 +1,7 @@
 roundor_url=https://ff.static.1001fonts.net/r/o/roundornoncommercial.regular.otf
 nunito_url=https://www.1001freefonts.com/d/5900/nunito.zip
 space_mono_url=https://ff.static.1001fonts.net/s/p/space-mono.regular.ttf
+space_mono_bold_url=https://ff.static.1001fonts.net/s/p/space-mono.bold.ttf
 
 wget --no-use-server-timestamps "$roundor_url" -O roundor.otf
 wget --no-use-server-timestamps "$nunito_url" -O nunito.zip
@@ -8,3 +9,4 @@ unzip nunito.zip Nunito-Regular.ttf
 mv Nunito-Regular.ttf nunito.ttf
 rm nunito.zip
 wget --no-use-server-timestamps "$space_mono_url" -O space_mono.ttf
+wget --no-use-server-timestamps "$space_mono_bold_url" -O space_mono_bold.ttf
