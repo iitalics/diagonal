711a6688370e238651c75a295c4a57ddfd52bea5
Milo Turner <iitalics@gmail.com>
2020-06-24 15:38:28 -0400

    turn bounding box

--- dia_ui/hud.ml
+++ dia_ui/hud.ml
@@ -33,16 +33,6 @@ module Make
     type assets = A
     let assets_rsrc = Rsrc.const A
 
-    (*** type defs ***)
-
-    type t =
-      { base_tf: Affine.t;
-        player_0: player_data;
-        player_1: player_data }
-
-    and player_data =
-      { pl_tf: Affine.t }
-
     (*** rendering ***)
 
     let bg_c = Color.(of_rgb_s "#000" |> with_alpha 0.56)
@@ -50,6 +40,7 @@ module Make
     (* HUD origin *)
 
     let base_y = 20
+    let padding = 12
 
     let update_base_tf (w, _) tf =
       tf |> Affine.reset;
@@ -57,9 +48,11 @@ module Make
 
     (* players *)
 
+    type player_data =
+      { pl_tf: Affine.t }
+
     let pl_bbox_w = 420
     let pl_bbox_h = 104
-    let pl_bbox_pad = 12
     let pl_bbox_xs, pl_bbox_ys =
       [| 0; pl_bbox_w; pl_bbox_w; 0 |],
       [| 0; 0; pl_bbox_h; pl_bbox_h |]
@@ -67,8 +60,8 @@ module Make
     let make_player_data base_tf idx =
       let tf = base_tf |> Affine.extend in
       tf |> Affine.translate_i
-              (pl_bbox_pad / 2
-               + (idx - 1) * (pl_bbox_w + pl_bbox_pad))
+              (padding / 2
+               + (idx - 1) * (pl_bbox_w + padding))
               0;
       { pl_tf = tf }
 
@@ -77,13 +70,45 @@ module Make
               ~t:pl_tf ~c:bg_c
               ~xs:pl_bbox_xs ~ys:pl_bbox_ys
 
+    (* turn *)
+
+    let tn_bbox_w = 176
+    let tn_bbox_h = 52
+    let tn_bbox_xs, tn_bbox_ys =
+      [| 0; tn_bbox_w; tn_bbox_w; 0 |],
+      [| 0; 0; tn_bbox_h; tn_bbox_h |]
+
+    type turn_data =
+      { tn_tf: Affine.t }
+
+    let make_turn_data base_tf =
+      let tf = base_tf |> Affine.extend in
+      tf |> Affine.translate_i
+              (-tn_bbox_w / 2)
+              (pl_bbox_h + padding);
+      { tn_tf = tf }
+
+    let render_turn_data ~cx { tn_tf } =
+      cx |> Ctxt.vertices `Fill
+              ~t:tn_tf ~c:bg_c
+              ~xs:tn_bbox_xs ~ys:tn_bbox_ys
+
+    (* entrypoint *)
+
+    type t =
+      { base_tf: Affine.t;
+        player_0: player_data;
+        player_1: player_data;
+        turn: turn_data }
+
     let render cx
-          { base_tf; player_0; player_1 }
+          { base_tf; player_0; player_1; turn }
       =
       begin
         base_tf |> update_base_tf (cx |> Ctxt.size);
         player_0 |> render_player_data ~cx;
         player_1 |> render_player_data ~cx;
+        turn |> render_turn_data ~cx;
       end
 
     (*** processing game state ***)
@@ -97,5 +122,6 @@ module Make
       let base_tf = Affine.make () in
       { base_tf;
         player_0 = make_player_data base_tf 0;
-        player_1 = make_player_data base_tf 1 }
+        player_1 = make_player_data base_tf 1;
+        turn = make_turn_data base_tf }
   end
