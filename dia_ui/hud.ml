dcc6acc2ea81f446cafc7e669bae006bdf1f0405
Milo Turner <iitalics@gmail.com>
2020-06-24 15:32:31 -0400

    HUD redo: player data bounding boxes

--- dia_ui/hud.ml
+++ dia_ui/hud.ml
@@ -1,7 +1,4 @@
 module Gameplay = Dia_game.Gameplay
-module Item_type = Dia_game.Item_type
-module Player = Dia_game.Player
-module Rules = Dia_game.Rules
 
 module type S = sig
   type assets
@@ -28,276 +25,77 @@ module Make
     type draw_ctxt = Draw.Ctxt.t
     type 'a rsrc = 'a Rsrc.t
 
-    module Rsrc = struct
-      include Rsrc
-      include Util.Applicative(Rsrc)
-    end
-
-    module Ctxt = Draw.Ctxt
     module Color = Draw.Color
-    module Font = Draw.Font
-    module Image = Draw.Image
+    module Ctxt = Draw.Ctxt
 
     (*** assets ***)
 
-    type assets =
-      { sprites:       Image.t;
-        p_name_font:   Font.t;
-        act_item_font: Font.t;
-        turn_font:     Font.t }
+    type assets = A
+    let assets_rsrc = Rsrc.const A
 
-    let assets_rsrc : assets Rsrc.t =
-      let fonts =
-        Rsrc.(all [ font ~family:"space_mono" ~size:24;
-                    font ~family:"space_mono" ~size:15;
-                    font ~family:"space_mono_bold" ~size:16 ])
-      in
-      let images =
-        Rsrc.image ~path:"sprites"
-      in
-      Rsrc.map2 (fun[@ocaml.warning "-8"]
-                    [ p_name_font; act_item_font; turn_font ]
-                    sprites ->
-          { sprites; p_name_font; act_item_font; turn_font })
-        fonts
-        images
-
-    (*** data ***)
+    (*** type defs ***)
 
     type t =
-      { assets: assets;
-        base_tf: Affine.t;
-        mutable player_0: player_data;
-        mutable player_1: player_data;
-        mutable turn_data: turn_data }
+      { base_tf: Affine.t;
+        player_0: player_data;
+        player_1: player_data }
 
     and player_data =
-      { pl_idx: int;
-        pl_name: string;
-        mutable pl_hp: int;
-        mutable pl_item: Item_type.t;
-        pl_name_tf: Affine.t;
-        pl_hpbar_tf: Affine.t;
-        pl_items_tf: Affine.t;
-        pl_item_icon_tf: Affine.t }
-
-    and turn_data =
-      { tn_tf: Affine.t;
-        mutable tn_num: int;
-        mutable tn_dur: float;
-        (* amt(t) = amt0 + t * amt_v *)
-        mutable tn_amt0: float;
-        mutable tn_amt_v: float;
-        mutable tn_text: string;
-        tn_bar_o: int array * int array;
-        tn_bar_f: int array * int array }
+      { pl_tf: Affine.t }
 
     (*** rendering ***)
 
-    let hud_bg_c = Color.(of_rgb_s "#000" |> with_alpha 0.5)
-    let hud_c    = Color.of_rgb_s "#fff"
-    let hud_w = 800
-    let hud_h = 128
-    let hud_y = 30
-    let hud_left = 9
-    let hud_top = 8
-    let hud_hpbar_fill_c = Color.[| of_rgb_s "#04f"; of_rgb_s "#f04" |]
-    let hud_hpbar_y = 36
-    let hud_hpbar_w = 386
-    let hud_hpbar_h = 20
-    let hud_item_x  = 56 (* 32 *)
-    let hud_item_y  = 84
-    let hud_item_scale = 0.8
-    let hud_item_act_text_c = Color.of_rgb_s "#ff0"
-    let hud_item_act_text_dy = 24
-    let hud_turn_x = hud_w / 2
-    let hud_turn_y = 78
-    let hud_turn_bar_w = 160
-    let hud_turn_bar_h = 12
-    let hud_turn_bar_dy = 20
-    let hud_turn_bar_fill_c = Color.(of_rgb_s "#fff" |> with_alpha 0.6)
+    let bg_c = Color.(of_rgb_s "#000" |> with_alpha 0.56)
 
-    (* background *)
+    (* HUD origin *)
+
+    let base_y = 20
 
     let update_base_tf (w, _) tf =
       tf |> Affine.reset;
-      tf |> Affine.translate_i ((w - hud_w) / 2) hud_y
+      tf |> Affine.translate_i (w / 2) base_y
 
-    let bg_xs, bg_ys = [| 0; hud_w; hud_w; 0     |],
-                       [| 0; 0;     hud_h; hud_h |]
+    (* players *)
 
-    let render_bg ~cx tf =
-      cx |> Ctxt.vertices `Fill
-              ~t:tf ~c:hud_bg_c
-              ~xs:bg_xs ~ys:bg_ys
+    let pl_bbox_w = 420
+    let pl_bbox_h = 104
+    let pl_bbox_pad = 12
+    let pl_bbox_xs, pl_bbox_ys =
+      [| 0; pl_bbox_w; pl_bbox_w; 0 |],
+      [| 0; 0; pl_bbox_h; pl_bbox_h |]
 
-    (* player info *)
-
-    let make_player_data base_tf Player.{ hp; item; color = i; _ } =
-      (* basic data *)
-      let name = [| "Player One"; "Player Two" |].(i) in
-      (* transformation matrices *)
-      let name_tf = base_tf |> Affine.extend in
-      let hpbar_tf = base_tf |> Affine.extend in
-      let items_tf = base_tf |> Affine.extend in
-      let item_icon_tf = items_tf |> Affine.extend in
-      name_tf |> Affine.translate_i
-                   ((1 - i) * hud_left
-                    +     i * (hud_w - hud_left))
-                   hud_top;
-      hpbar_tf |> Affine.translate_i
-                    ((1 - i) * hud_left
-                     +     i * (hud_w - hud_hpbar_w - hud_left))
-                    hud_hpbar_y;
-      items_tf |> Affine.translate_i
-                    ((1 - i) * hud_item_x
-                     +     i * (hud_w - hud_item_x))
-                    hud_item_y;
-      item_icon_tf |> Affine.scale
-                        hud_item_scale hud_item_scale;
-      { pl_idx = i;
-        pl_name = name;
-        pl_hp = hp;
-        pl_item = item;
-        pl_name_tf = name_tf;
-        pl_hpbar_tf = hpbar_tf;
-        pl_items_tf = items_tf;
-        pl_item_icon_tf = item_icon_tf }
-
-    let update_player_data time0 (pl: Player.t) (pl_data: player_data) =
-      ignore time0;
-      pl_data.pl_hp <- pl.hp;
-      pl_data.pl_item <- pl.item
-
-    let render_item_icon_img ~assets ~cx (typ: Item_type.t) tf =
-      let sx = 352 + 64 * Item_type.to_int typ in
-      cx |> Ctxt.image assets.sprites
-              ~x:(-32) ~y:(-32) ~t:tf
-              ~sx ~sy:0 ~w:64 ~h:64
-
-    let render_player ~assets ~cx
-          { pl_idx; pl_name; pl_hp; pl_item;
-            pl_name_tf; pl_hpbar_tf; pl_items_tf; pl_item_icon_tf }
-      =
-      (* player name *)
-      (let font = assets.p_name_font in
-       let (mes_w, _) = font |> Font.measure pl_name in
-       cx |> Ctxt.text pl_name
-               ~c:hud_c ~font ~t:pl_name_tf
-               ~x:(pl_idx * -mes_w) ~y:0 );
-
-      (* hp bar *)
-      (let w  = hud_hpbar_w in
-       let w' = hud_hpbar_w * pl_hp / Rules.max_hp in
-       let h  = hud_hpbar_h in
-       cx |> Ctxt.vertices `Fill
-               ~c:hud_hpbar_fill_c.(pl_idx) ~t:pl_hpbar_tf
-               ~xs:[| 0; w'; w'; 0 |]
-               ~ys:[| 0; 0; h; h |];
-       cx |> Ctxt.vertices `Strip
-               ~c:hud_c ~t:pl_hpbar_tf
-               ~xs:[| 0; w; w; 0; 0 |]
-               ~ys:[| 0; 0; h; h; 0 |]);
-
-      (* items *)
-      (pl_item_icon_tf |> render_item_icon_img ~assets ~cx pl_item;
-       let font = assets.act_item_font in
-       let text = pl_item |> Item_type.name in
-       let (mes_w, _) = font |> Font.measure text in
-       cx |> Ctxt.text text
-               ~font ~c:hud_item_act_text_c ~t:pl_items_tf
-               ~x:(-mes_w / 2)
-               ~y:hud_item_act_text_dy)
-
-    (* turn data *)
-
-    let turn_bar_coords amt =
-      let x0, y0 = -hud_turn_bar_w / 2, hud_turn_bar_dy in
-      let w, h = int_of_float (float_of_int hud_turn_bar_w *. amt), hud_turn_bar_h in
-      x0, y0, x0 + w, y0 + h
-
-    let make_turn_data base_tf =
+    let make_player_data base_tf idx =
       let tf = base_tf |> Affine.extend in
       tf |> Affine.translate_i
-              hud_turn_x
-              hud_turn_y;
-      let x0, y0, x1, y1 = turn_bar_coords 1. in
-      { tn_tf = tf;
-        tn_num = 0; tn_dur = 1.;
-        tn_amt0 = 0.; tn_amt_v = 0.;
-        tn_text = "";
-        tn_bar_o = [| x0; x1; x1; x0; x0 |], [| y0; y0; y1; y1; y0 |];
-        tn_bar_f = [| x0; x1; x1; x0 |],     [| y0; y0; y1; y1 |] }
+              (pl_bbox_pad / 2
+               + (idx - 1) * (pl_bbox_w + pl_bbox_pad))
+              0;
+      { pl_tf = tf }
 
-    let start_turn time0 ~dur tn =
-      (* amt(t0)      = 1
-         amt(t0 + dt) = 0 *)
-      tn.tn_dur <- dur;
-      tn.tn_amt_v <- -1. /. dur;
-      tn.tn_amt0 <- 1. -. time0 *. tn.tn_amt_v
+    let render_player_data ~cx { pl_tf } =
+      cx |> Ctxt.vertices `Fill
+              ~t:pl_tf ~c:bg_c
+              ~xs:pl_bbox_xs ~ys:pl_bbox_ys
 
-    let update_turn time
-          ({ tn_num; tn_dur; tn_amt0; tn_amt_v; _ } as tn)
+    let render cx
+          { base_tf; player_0; player_1 }
       =
-      let tn_amt = max 0. (tn_amt0 +. time *. tn_amt_v) in
-      let tn_time = tn_amt *. tn_dur in
-      let fill_xs, _ = tn.tn_bar_f in
-      let _, _, fill_x1, _ = turn_bar_coords tn_amt in
-      tn.tn_text <- Printf.sprintf "turn %d (%.1fs)" tn_num tn_time;
-      fill_xs.(1) <- fill_x1;
-      fill_xs.(2) <- fill_x1
-
-    let render_turn_indicator ~assets ~cx
-          { tn_tf; tn_text; tn_bar_o; tn_bar_f; _ }
-      =
-      (let font = assets.turn_font in
-       let (mes_w, _) = font |> Font.measure tn_text in
-       cx |> Ctxt.text tn_text
-               ~font ~c:hud_c ~t:tn_tf
-               ~x:(-mes_w / 2) ~y:0);
-
-      (let outl_xs, outl_ys = tn_bar_o in
-       let fill_xs, fill_ys = tn_bar_f in
-       cx |> Ctxt.vertices `Fill
-               ~c:hud_turn_bar_fill_c ~t:tn_tf
-               ~xs:fill_xs ~ys:fill_ys;
-       cx |> Ctxt.vertices `Strip
-               ~c:hud_c ~t:tn_tf
-               ~xs:outl_xs ~ys:outl_ys)
-
-    let render cx { assets; base_tf; player_0; player_1; turn_data; _ } : unit =
       begin
         base_tf |> update_base_tf (cx |> Ctxt.size);
-        base_tf |> render_bg ~cx;
-        player_0 |> render_player ~assets ~cx;
-        player_1 |> render_player ~assets ~cx;
-        turn_data |> render_turn_indicator ~assets ~cx;
+        player_0 |> render_player_data ~cx;
+        player_1 |> render_player_data ~cx;
       end
 
-    (*** processing game state data ***)
+    (*** processing game state ***)
 
-    let update time hud =
-      hud.turn_data |> update_turn time
-
-    let update_game time0 game hud =
-      begin
-        (* turn *)
-        hud.turn_data.tn_num <- game |> Gameplay.turn_num;
-        game |> Gameplay.turn_duration |>
-          Option.iter (fun dur ->
-              hud.turn_data |> start_turn time0 ~dur);
-        (* players *)
-        hud.player_0 |> update_player_data time0 (game |> Gameplay.player_0);
-        hud.player_1 |> update_player_data time0 (game |> Gameplay.player_1);
-      end
+    let update _time _hud = ()
+    let update_game _time0 _g _hud = ()
 
     (*** init ***)
 
-    let make assets game =
+    let make _assets _g =
       let base_tf = Affine.make () in
-      { assets;
-        base_tf;
-        player_0 = game |> Gameplay.player_0 |> make_player_data base_tf;
-        player_1 = game |> Gameplay.player_1 |> make_player_data base_tf;
-        turn_data = make_turn_data base_tf }
+      { base_tf;
+        player_0 = make_player_data base_tf 0;
+        player_1 = make_player_data base_tf 1 }
   end
