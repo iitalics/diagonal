7e2ecfa2e9921cd3839fd5feaf01b028ffd76892
Milo Turner <iitalics@gmail.com>
2020-07-01 22:04:37 -0400

    on pick up, move current item to alt slot

--- dia_game/player.ml
+++ dia_game/player.ml
@@ -17,8 +17,11 @@ module Equip = struct
     else
       t
 
-  let set_spell s t =
-    { t with spell = Some s; casts_left = Rules.max_casts }
+  let set_spell ?(casts=Rules.max_casts) s t =
+    if Option.is_some s then
+      { t with spell = s; casts_left = casts }
+    else
+      t
 
   let set_weapon w t =
     normalize { t with weapon = w }
@@ -45,12 +48,12 @@ let take_damage dmg pl =
 let pick_up item pl =
   match item with
   | Item_type.Weapon w ->
-     { pl with prm = pl.prm |> Equip.set_weapon w
-       (* TODO: move item into alt slot *) }
-
+     { pl with prm = pl.prm |> Equip.set_weapon w;
+               alt = pl.alt |> Equip.set_weapon pl.prm.weapon }
   | Item_type.Spell s  ->
-     { pl with prm = pl.prm |> Equip.set_spell s;
-       (* TODO: move item into alt slot *) }
+     { pl with prm = pl.prm |> Equip.set_spell (Some s);
+               alt = pl.alt |> Equip.set_spell pl.prm.spell
+                                 ~casts:pl.prm.casts_left }
 
 let use_spell_cast pl =
   match pl.prm.spell with
